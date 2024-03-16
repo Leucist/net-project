c5f6cca2e3959c9a5e9f66211cf75d4c296468f9
leucistic <leucist@yandex.by>
2024-03-16 14:21:34 +0100

    Synchronising the repo

--- obj/Debug/net8.0/Educational_platform.AssemblyInfo.cs
+++ obj/Debug/net8.0/Educational_platform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Educational_platform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bc89e6100ed5904d942778e9d9f52e7b23787df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1ea00e2f5c7b879c50ab7f91dddf9de33806a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Educational_platform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Educational_platform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
