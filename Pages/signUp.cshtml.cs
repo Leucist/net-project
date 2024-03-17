d25a927bfa7a7b97f8fba507360298e33798c46c
= <mr.multik.zzz@gmail.com>
2024-03-17 21:21:03 +0100

    Added sign up page instead of making modal window

---
+++ Pages/signUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Educational_platform.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
