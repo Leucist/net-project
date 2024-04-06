ea0ac9ab9accc3142350faa41b520b4b486b6620
leucistic <leucist@yandex.by>
2024-04-06 17:32:20 +0200

    Implemented AccountController

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,28 +5,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Educational_platform.Data;
+using Educational_platform.Models;
 
 namespace Educational_platform.Controllers
 {
-    [Route("[controller]")]
     public class AccountController : Controller
     {
-        private readonly ILogger<AccountController> _logger;
+        private readonly UsersContext _context;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(UsersContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        [HttpPost]
+        public async Task<IActionResult> SignUp(string name, string surname, string email, string username, string password)
+        {
+            // Check if the user with the given username or email already exists
+            var existingUser = await _context.Users
+                .Where(u => u.Username == username || u.Email == email)
+                .FirstOrDefaultAsync();
+
+            if (existingUser != null)
+            {
+                // User with such username or email already exists
+                ModelState.AddModelError(string.Empty, "A user with the same username or email already exists.");
+                return View();
+            }
+
+            // Create new user
+            var newUser = new Users
+            {
+                Name = name,
+                Surname = surname,
+                Email = email,
+                Username = username,
+                Password = password 
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            // Redirect to the SignIn
+            return RedirectToAction("SignIn", "Account");
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View("Error!");
         }
     }
 }
