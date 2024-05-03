a613875baa18f33d196ddf2c2ce80ca3cdc7c872
grinnazar <grinnazar@gmail.com>
2024-05-03 11:29:03 +0200

    Fixed BD, added migration

--- Migrations/20240523091052_FinalRelationshipV2.Designer.cs
+++ Migrations/20240523091052_FinalRelationshipV2.Designer.cs
@@ -3,6 +3,7 @@ using Educational_platform.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Educational_platform.Migrations
 {
     [DbContext(typeof(UsersContext))]
-    partial class UsersContextModelSnapshot : ModelSnapshot
+    [Migration("20240523091052_FinalRelationshipV2")]
+    partial class FinalRelationshipV2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
