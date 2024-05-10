791471f165f9d158f4d259a94d0f76f9387fe891
= <mr.multik.zzz@gmail.com>
2024-05-10 14:32:01 +0200

    Added catalogue page

---
+++ Pages/Catalogue.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Educational_platform.Pages
+{
+    public class CatalogueModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
