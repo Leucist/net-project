89bc86c64bfe6023cb0b3d05190a33397beb6c05
grinnazar <grinnazar@gmail.com>
2024-05-25 16:53:37 +0200

    Changed DB location

--- Program.cs
+++ Program.cs
@@ -3,8 +3,12 @@ using Educational_platform.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+AppDomain.CurrentDomain.SetData("DataDirectory", Path.Combine(Directory.GetCurrentDirectory(), "Database"));
+
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 
 // Add the database context configuration here
 builder.Services.AddDbContext<UsersContext>(options =>
