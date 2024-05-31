a7c436d7e1e34d8a371c849a20afa48c9d1377bf
leucistic <leucist@yandex.by>
2024-06-01 00:16:26 +0200

    Added course page, updated Catalogue

---
+++ Pages/CoursePages/Course.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Educational_platform.Data;
+using Educational_platform.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Educational_platform.Pages.CoursePages
+{
+    public class Course : PageModel
+    {
+        private readonly UsersContext _context;
+
+        public Course(UsersContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Courses? CourseOnPage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            CourseOnPage = await _context.Courses
+                .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (CourseOnPage == null)
+            {
+                return RedirectToPage("../Error");
+            }
+
+            return Page();
+        }
+    }
+}
